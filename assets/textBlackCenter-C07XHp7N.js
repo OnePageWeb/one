c9b468e8dc8b76c1003208903cd1685b83cec34c
Verlif <1650825929@qq.com>
2025-08-26 14:03:44 +0000

    Deploying to gh-pages from @ Verlif/another-homepage@639c968350bddbfd3b57064352b2ad90ad9f0661 🚀

---
+++ assets/textBlackCenter-C07XHp7N.js
@@ -0,0 +1,13 @@
+const t="黑底白字",n="在组件中显示居中的文字",e='{"text":""}',c=`.result {
+  color: white;
+  font-weight: bold;
+  padding: 8px 8px;
+  background: #2d2d2dbf;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% - 16px);
+  height: calc(100% - 16px);
+  backdrop-filter: blur(4px);
+}`,o="text",d={name:t,desc:n,config:e,style:c,type:o};export{e as config,d as default,n as desc,t as name,c as style,o as type};
